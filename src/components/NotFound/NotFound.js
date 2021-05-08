f0c9edc5e75af2aad59465e89e0aeb1b6d40cae0
jarekTerejko <j.terejko@gmail.com>
2021-05-08 05:00:38 +0200

    display error value

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { RestCountriesContext } from "../../contexts/RestCountriesContext";
 
 const NotFound = () => {
+  const {
+    countryError,
+    countriesError,
+    errorCountriesValue,
+    errorCountryValue,
+  } = useContext(RestCountriesContext);
   return (
     <div
       style={{
         height: "100vh",
+        flexDirection: "column",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      <h1 style={{ fontSize: "4rem", color: "var(--Very-Dark-Blue)" }}>
+      <h1 style={{ fontSize: "7rem", color: "red" }}>404</h1>
+      {countryError && (
+        <p style={{ fontSize: "1.6rem", color: "red", marginBottom: "2rem" }}>
+          {errorCountryValue}
+        </p>
+      )}
+      {countriesError && (
+        <p style={{ fontSize: "1.6rem", color: "red", marginBottom: "2rem" }}>
+          {errorCountriesValue}
+        </p>
+      )}
+      <h2
+        style={{
+          fontSize: "4rem",
+          color: "var(--Very-Dark-Blue)",
+          marginBottom: "3rem",
+        }}
+      >
         This page does not exists
-      </h1>
+      </h2>
+      <Link style={{ fontSize: "2rem" }} to="/" className="btn btn--border">
+        Home
+      </Link>
     </div>
   );
 };
